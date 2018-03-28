d69e1398d2fb077da525918fb8e10bc54604944a
max_schuessler <mschuessler@unomaha.edu>
2018-03-27 21:28:55 -0500

    Imported elmwood and began traffic controller. Man I really don't wanna do this. kms

---
+++ Art4190/Assets/Main/Scripts/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+	public Vector3 nextDest;
+	public bool stopped;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!stopped)
+			transform.Translate((transform.position - nextDest).normalized);
+		
+	}
+}
---
+++ Art4190/Assets/Main/Scripts/TrafficController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficController : MonoBehaviour {
+
+	public GameObject[] spawnLocs, deSpawnLocs, stopLocs;
+	public GameObject carPrefab, carParent;
+	public float spawnInterval;
+
+
+
+	// Use this for initialization
+	void Start () {
+		spawnInterval = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator SpawnCar() {
+		yield return new WaitForSeconds(spawnInterval);
+
+		GameObject currCar = Instantiate(carPrefab, spawnLocs[Random.Range(0, 3)].transform.position, Quaternion.identity, carParent.transform);
+		//Set rotation of car
+
+		//Add to queue
+
+		//Set stop location
+
+	}
+}
