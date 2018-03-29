68a9d3c12f4400f6d54c797e808879ef0254e1bf
max_schuessler <mschuessler@unomaha.edu>
2018-03-28 23:34:48 -0500

    Almost done with the traffic controller for elmwood. Just need to fix 1 bug and loop the car spawning.

--- Art4190/Assets/Main/Scripts/Car.cs
+++ Art4190/Assets/Main/Scripts/Car.cs
@@ -6,16 +6,42 @@ public class Car : MonoBehaviour {
 
 	public Vector3 nextDest;
 	public bool stopped;
+	public int lane;
+
+	private GameObject gc;
+	TrafficController tc;
 
 	// Use this for initialization
 	void Start () {
+		gc = GameObject.FindWithTag("GameController");
+		tc = gc.GetComponent<TrafficController>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (!stopped) { 
+			//transform.Translate((transform.position - nextDest).normalized);
+			transform.position = Vector3.MoveTowards(transform.position, nextDest, .1f);
+			if(Vector3.Distance(nextDest, transform.position) <= 0.1f) {
+				stopped = true;
+				tc.stopQ.Enqueue(gameObject);
+			}
+		}
+		else {
+			if(tc.stopQ.Count > 0 && tc.stopQ.Peek() == gameObject) {
+				StartCoroutine(Stop());
+			}
+		}
+
+		
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(!stopped)
-			transform.Translate((transform.position - nextDest).normalized);
-		
+
+	private IEnumerator Stop() {
+		yield return new WaitForSeconds(3f);
+		if (tc.stopQ.Count > 0) {
+			tc.stopQ.Dequeue();
+			tc.lanes[lane].Dequeue();
+		}
 	}
 }
--- Art4190/Assets/Main/Scripts/TrafficController.cs
+++ Art4190/Assets/Main/Scripts/TrafficController.cs
@@ -7,12 +7,25 @@ public class TrafficController : MonoBehaviour {
 	public GameObject[] spawnLocs, deSpawnLocs, stopLocs;
 	public GameObject carPrefab, carParent;
 	public float spawnInterval;
-
+	public Queue<GameObject> lane1, lane2, lane3, lane4, stopQ;
+	public Queue<GameObject>[] lanes;
 
 
 	// Use this for initialization
 	void Start () {
 		spawnInterval = 3f;
+		lane1 = new Queue<GameObject>();
+		lane2 = new Queue<GameObject>();
+		lane3 = new Queue<GameObject>();
+		lane4 = new Queue<GameObject>();
+		stopQ = new Queue<GameObject>();
+		lanes = new Queue<GameObject>[4];
+		lanes[0] = lane1;
+		lanes[1] = lane2;
+		lanes[2] = lane3;
+		lanes[3] = lane4;
+
+		StartCoroutine(SpawnCar());
 	}
 	
 	// Update is called once per frame
@@ -22,13 +35,39 @@ public class TrafficController : MonoBehaviour {
 
 	private IEnumerator SpawnCar() {
 		yield return new WaitForSeconds(spawnInterval);
-
-		GameObject currCar = Instantiate(carPrefab, spawnLocs[Random.Range(0, 3)].transform.position, Quaternion.identity, carParent.transform);
-		//Set rotation of car
-
+		print("Sparning car");
+		int rand = Random.Range(0, 3);
+		GameObject currCar = Instantiate(carPrefab, spawnLocs[rand].transform.position, Quaternion.identity, carParent.transform);
+		Car car = currCar.GetComponent<Car>();
+		car.lane = rand;
 		//Add to queue
+		lanes[rand].Enqueue(currCar);
 
 		//Set stop location
+		Vector3 stopDest = stopLocs[rand].transform.position;
+		//Set rotation of car
+		switch (rand) {
+			case 0:
+				currCar.transform.rotation = Quaternion.Euler(new Vector3(0, 180f, 0));
+				stopDest.x -= (lanes[rand].Count * 3f);
+				break;
+			case 1:
+				currCar.transform.rotation = Quaternion.Euler(new Vector3(0, 90f, 0));
+				stopDest.z -= (lanes[rand].Count * 3f);
+				break;
+			case 2:
+				currCar.transform.rotation = Quaternion.Euler(new Vector3(0, 270f, 0));
+				stopDest.z += (lanes[rand].Count * 3f);
+				break;
+			case 3:
+				currCar.transform.rotation = Quaternion.Euler(Vector3.zero);
+				stopDest.x += (lanes[rand].Count * 3f);
+				break;
+			default:
+				Debug.Log("No lane at: " + rand);
+				break;
+		}
+		car.nextDest = stopDest;
 
 	}
 }
