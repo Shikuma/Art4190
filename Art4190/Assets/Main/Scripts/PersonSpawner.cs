ea310429e6334c271e62428b05ae69ecb3adb489
Zach 'Leif' Eriksen <zeriksen@clientresourcesinc.com>
2018-04-18 18:56:00 -0500

    Started work on the time of day and dynamic person spawning

--- Art4190/Assets/Main/Scripts/PersonSpawner.cs
+++ Art4190/Assets/Main/Scripts/PersonSpawner.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 
 public class PersonSpawner : MonoBehaviour {
 	public GameObject personPrefab, parentObject, destinationObject;
-	[SerializeField]
-	float spawnInterval, spawnMin, spawnMax;
 	public GameObject[] spawnLocations, destinationLocations;
 
 	// Use this for initialization
 	void Start () {
-		
 		StartCoroutine(SpawnPerson());
 	}
 
 	private IEnumerator SpawnPerson() {
-		spawnInterval = Random.Range(spawnMin, spawnMax);
-		yield return new WaitForSeconds(spawnInterval);
+		float time = GameObject.FindWithTag("GameController").GetComponent<DayNightController>().currentTimeOfDay;
+		yield return new WaitForSeconds(Mathf.Abs(time - 0.5f) * 10);
+
 		int rand = Random.Range(0, spawnLocations.Length);
 		GameObject person = Instantiate(personPrefab, spawnLocations[rand].transform.position, Quaternion.identity, parentObject.transform);
 		PersonController pc = person.GetComponent<PersonController>();
 		pc.goalObject = destinationLocations[rand];
-
-		if (spawnMin > 1f) spawnMin--;
-		if (spawnMax > 5f) spawnMax--;
 
 		StartCoroutine(SpawnPerson());
 	}
