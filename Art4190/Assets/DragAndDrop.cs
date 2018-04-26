fc8806850f685c15edd35d7b6a8d12a6b2e3a905
max_schuessler <mschuessler@unomaha.edu>
2018-04-25 21:07:15 -0500

    finished dragging and placing objects (stop signs and street lights)

--- Art4190/Assets/DragAndDrop.cs
+++ Art4190/Assets/DragAndDrop.cs
@@ -6,38 +6,53 @@ public class DragAndDrop : MonoBehaviour {
 	public GameObject[] standardObjects, userObjects;
 	private bool isDragging = false, isStopSignSelected, isLightPoleSelected;
 	private float yPos;
-	private GameObject draggingObject;
-	public GameObject stopSign, streetLight, flower;
+	private GameObject draggingObject, selectedObject;
+	public GameObject stopSign, streetLight, flower, removeButton;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(isDragging) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
-			if(Input.GetMouseButtonDown(0)) {
-				isDragging = false;
-				isStopSignSelected = false;
-			}else{
-				if(isStopSignSelected) {
-					draggingObject = Instantiate(stopSign, Vector3.zero, Quaternion.identity);
+			
+		if (Physics.Raycast(ray, out hit)) {
+			if (isDragging) {
+				print(hit.transform.gameObject.name);
+				if (Input.GetMouseButtonDown(0)) {
+					isDragging = false;
 					isStopSignSelected = false;
-					yPos = 1f;
-				}else if(isLightPoleSelected){
-					draggingObject = Instantiate(streetLight, Vector3.zero, Quaternion.identity);
 					isLightPoleSelected = false;
-					yPos = 2f;
+					if ((hit.transform.gameObject.tag == "stopSign" && draggingObject.gameObject.tag == "stopSign") || (hit.transform.gameObject.tag == "lightPole" && draggingObject.gameObject.tag == "lightPole")) {
+						hit.transform.GetChild(0).gameObject.SetActive(true);
+					}
+					Destroy(draggingObject);
 				}
-			}
-            if (Physics.Raycast(ray, out hit)) {
+				else {
+					if (isStopSignSelected) {
+						draggingObject = Instantiate(stopSign, Vector3.zero, Quaternion.identity);
+						isStopSignSelected = false;
+						yPos = 1f;
+					}
+					else if (isLightPoleSelected) {
+						draggingObject = Instantiate(streetLight, Vector3.zero, Quaternion.identity);
+						isLightPoleSelected = false;
+						yPos = 2f;
+					}
+				}
+
+				if(draggingObject != null)
 				draggingObject.transform.position = new Vector3(hit.point.x,
 																yPos,
 																hit.point.z);
-            }
-			
+			}else if (Input.GetMouseButtonDown(0)) {
+				if(hit.transform.gameObject.tag == "stopSign" || hit.transform.gameObject.tag == "lightPole") {
+					selectedObject = hit.transform.gameObject;
+					removeButton.SetActive(true);
+				}
+				print(hit.transform.gameObject.name);
+			}	
 		}
 	}
 
@@ -49,5 +64,10 @@ public class DragAndDrop : MonoBehaviour {
 	public void lightPoleCreate() {
 		isDragging = true;
 		isLightPoleSelected = true;
+	}
+
+	public void RemoveSelected() {
+		selectedObject.transform.GetChild(0).gameObject.SetActive(false);
+		removeButton.SetActive(false);
 	}
 }
