b875d8c20197d8c8ee5b8d0df723d00b060a5605
max_schuessler <mschuessler@unomaha.edu>
2018-05-02 17:50:03 -0500

    dialogue started for zach

---
+++ Art4190/Assets/Main/Scripts/Dialogue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+	public Text dialogueText;
+	public string[] goodFeedback, badFeedback, neutralFeedback;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
